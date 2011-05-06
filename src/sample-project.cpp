12eedbc4a10874ef43a3122fc156edd4a6632981
matt.todd <matt.todd@asolutions.com>
2011-05-06 08:05:57 -0500

    initial

---
+++ src/sample-project.cpp
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : sample-project.cpp
+ Author      : Matt Todd
+ Version     :
+ Copyright   : 
+ Description : Hello World in C++,
+ ============================================================================
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	cout << "Hello World" << endl; /* prints Hello World */
+	return 0;
+}

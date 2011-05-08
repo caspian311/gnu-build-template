16e3f2bf9687448288d7bf7618a4a79aef19ecbf
matt.todd <matt.todd@asolutions.com>
2011-05-08 06:22:36 -0500

    did this a bit too many times so i made a script to do it.

---
+++ cleanup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+make distclean
+rm -fR COPYING INSTALL Makefile Makefile.in aclocal.m4 autom4te.cache/ config.guess config.log config.status config.sub configure install-sh missing src/Makefile src/Makefile.in
+

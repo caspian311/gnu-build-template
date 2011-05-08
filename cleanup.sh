022b012d2e4857bea2c33b588318c4fc15e85b2d
matt.todd <matt.todd@asolutions.com>
2011-05-08 06:39:49 -0500

    added more to the cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 make distclean
-rm -fR COPYING INSTALL Makefile Makefile.in aclocal.m4 autom4te.cache/ config.guess config.log config.status config.sub configure install-sh missing src/Makefile src/Makefile.in
+rm -fR COPYING INSTALL Makefile Makefile.in aclocal.m4 autom4te.cache/ config.guess config.log config.status config.sub configure install-sh missing src/Makefile src/Makefile.in depcomp src/hello/Makefile.in src/shared/Makefile.in
 
